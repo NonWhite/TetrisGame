45fea9a917d999e866c86cbc7a40b5ba2703f792
NonWhite <wperezurcia@gmail.com>
2015-10-13 14:22:10 -0300

    initial commit

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils ;
+
+public class Piece : MonoBehaviour {
+	
+	private float xmin = Constants.BX_MIN ;
+	private float xmax = Constants.BX_MAX ;
+	private float ymin = Constants.BY_MIN ;
+	private float ymax = Constants.BY_MAX ;
+
+	void Start(){
+		Vector3 position = Constants.roundVector (transform.position);
+		transform.position = position;
+	}
+
+	void Update () {
+		if( Input.GetKeyDown( KeyCode.DownArrow ) ){
+			Vector3 newPos = transform.position + Constants.DOWN_MOV ;
+			if( isValid( newPos ) )
+				transform.position = newPos ;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Vector3 newPos = transform.position + Constants.RIGHT_MOV ;
+			if( isValid( newPos ) )
+				transform.position = newPos ;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Vector3 newPos = transform.position + Constants.LEFT_MOV ;
+			if( isValid( newPos ) )
+				transform.position = newPos ;
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.Rotate ( 0 , 0 , 90 ) ;
+		}
+	}
+
+	bool isValid( Vector3 pos ){
+		bool xValid = xmin <= pos.x && pos.x <= xmax;
+		bool yValid = ymin <= pos.y && pos.y <= ymax;
+		return xValid && yValid;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils ;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] possiblePieces ;
+
+	private Vector3 initPos ;
+	private int idPiece ;
+	
+	void Start () {
+		Spawn () ;
+	}
+
+	void Update () {
+//		if (Input.GetMouseButtonDown (0)) {
+//			Spawn() ;
+//		}
+	}
+
+	void GetRandomPiece(){
+		idPiece = Random.Range (0, possiblePieces.Length) ;
+	}
+
+	void GetRandomPosition(){
+		int x = Random.Range (Constants.SX_MIN, Constants.SX_MAX);
+		initPos = new Vector3 (x, Constants.SPAWN_Y, 0);
+		initPos = Constants.roundVector (initPos);
+	}
+
+	void Spawn(){
+		GetRandomPiece ();
+		GetRandomPosition ();
+		GameObject piece = possiblePieces[ idPiece ] ;
+		Instantiate (piece, initPos, Quaternion.identity);
+	}
+}
